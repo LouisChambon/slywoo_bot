d88cf3e6ad0608342178819e15cc26b057205c06
Slywoo2 <louis.chambon@epitech.eu>
2022-10-26 15:05:31 +0200

    added join command implementation

---
+++ Commands/join.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: "join",
+    description: "Join voice channel",
+    permission: Discord.PermissionFlagsBits.Connect,
+    dm: false,
+
+    async run(client, message) {
+
+        
+    }
+}
